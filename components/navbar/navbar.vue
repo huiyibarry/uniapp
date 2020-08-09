734db677b5237b64faa80a7392aba8328d2473f3
huiyibarry <1373949185@qq.com>
2020-08-09 20:49:38 +0800

    顶部修改

--- components/navbar/navbar.vue
+++ components/navbar/navbar.vue
@@ -5,12 +5,14 @@
 				<view :style="{height:statusBarHeight+'px'}"></view>
 				<view class="navbar-content" :style="{height:navBarHeight+'px',width:windowWidth+'px'}">
 					<view class="navbar-serach">
-						<view class="navbar-serach_icon"></view>
+						<view class="navbar-serach_icon">
+							<uni-icons type="search" size="18" color="#999"></uni-icons>
+						</view>
 						<view class="navbar-serach_text">uni-app、vue</view>
 					</view>	
 				</view>				
 			</view>
-			<view style="height: 45px;"></view>
+			<view :style="{height:fixedHeight+'px'}"></view>
 		</view>
 </template>
 
@@ -18,20 +20,23 @@
 	export default {
 		data() {
 			return {
-				statusBarHeight : 20,
+				statusBarHeight: 20,
 				navBarHeight: 45,
-				windowWidth: 375
+				windowWidth: 375,
+				fixedHeight:45
 			};
 		},
 		created() {
 			const info = uni.getSystemInfoSync()
 			this.statusBarHeight = info.statusBarHeight;
+			this.windowWidth = info.windowWidth
 			//#ifndef APP-PLUS || H5 ||MP-ALIPAY
 			//小程序类悬浮按钮（胶囊按钮）
 			const  menuButtonInfo = uni.getMenuButtonBoundingClientRect()
 			// (胶囊底部高度 - 状态栏的高度) + (胶囊顶部高度 - 状态栏内的高度) = 导航栏的高度
 			this.navBarHeight = (menuButtonInfo.bottom - info.statusBarHeight) + (menuButtonInfo.top - info.statusBarHeight)
 			this.windowWidth = menuButtonInfo.left
+			this.fixedHeight = this.navBarHeight + this.statusBarHeight
 			//#endif
 		}
 	}
@@ -46,7 +51,6 @@
 			z-index: 99;
 			width: 100%;
 			background-color: $html-base-color;
-
 			.navbar-content {
 				display: flex;
 				justify-content: center;
@@ -54,7 +58,6 @@
 				padding: 0 15px;
 				height: 45px;
 				box-sizing: border-box;
-
 				.navbar-serach {
 					display: flex;
 					align-items: center;
@@ -63,14 +66,9 @@
 					height: 30px;
 					border-radius: 30px;
 					background-color: #fff;
-
-					.navbar-serach_icon {
-						width: 10px;
-						height: 10px;
-						border: 1px red solid;
+					.navbar-serach_icon {						
 						margin-right: 10px;
 					}
-
 					.navbar-serach_text {
 						font-size: 12px;
 						color: #999;
--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -1,34 +1,30 @@
 <template>
 	<view class="content">
+		<!-- 自定义导航栏 -->
 		<navbar></navbar>
 		<view v-for="item in 100">
-			{{item}}
+			{{item}} 内容那个
 		</view>
 	</view>
 </template>
 
 <script>
+	// easyCom components/组件名/组件名.vue 局部引入
 	export default {
 		data() {
 			return {
-				title: 'Hello',
-				src:''
+				title: 'Hello'
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-		
+
 		}
 	}
 </script>
 
 <style lang="scss">
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+	
 </style>
